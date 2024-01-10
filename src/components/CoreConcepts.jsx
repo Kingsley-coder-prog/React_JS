eb425bdd84144986784f599fe2ab13c7bd13933e
BlockGem <kifeanyi216@gmail.com>
2024-01-10 17:47:04 +0100

    Splitting Components By Feature & State

--- src/components/CoreConcepts.jsx
+++ src/components/CoreConcepts.jsx
@@ -1,1 +1,15 @@
-export default function CoreConcepts() {}
+import CoreConcept from "./CoreConcept";
+import { CORE_CONCEPTS } from "../data";
+
+export default function CoreConcepts() {
+  return (
+    <section id="core-concepts">
+      <h2>Core Concepts</h2>
+      <ul>
+        {CORE_CONCEPTS.map(conceptItem => (
+          <CoreConcept key={conceptItem.title} {...conceptItem} />
+        ))}
+      </ul>
+    </section>
+  );
+}
